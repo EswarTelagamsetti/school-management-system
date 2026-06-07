2c3dde0713ec2422c78030bce79e60278b96e307
[Eswar Telagamsetti] <eswartelagamsetti1234@gmail.com>
2026-06-07 10:00:18 +0530

    added database connection

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,23 @@
-DATABASE_URL = ""
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://root:password@localhost/school_management_db",
+)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
